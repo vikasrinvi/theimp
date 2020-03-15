1b6684f9e70be747c19e936b0aaf5559c2b91345
Shakil Alam <itxshakiil@gmail.com>
2020-03-15 17:45:08 +0530

    Some Ui Fixes for Mobile

--- resources/js/components/Copy.vue
+++ resources/js/components/Copy.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="flex w-full">
       <input
-        class="border flex-grow px-5 py-3 rounded-full m-2 -mr-2 rounded-r-none outline-none border-none text-gray-900"
+        class="border flex-grow pl-4 pr-2 md:px-5 py-3 rounded-full m-2 -mr-2 rounded-r-none outline-none border-none text-gray-900"
         type="url"
         name="url"
         id="url"
@@ -10,29 +10,29 @@
         v-model="link.shortenLink"
       />
       <input
-        class="cursor-pointer border px-5 py-3 rounded-full m-2 bg-blue-400 outline-none border-none text-gray-100 rounded-l-none"
+        class="cursor-pointer borde px-2 md:px-5 py-3 rounded-full m-2 bg-blue-400 outline-none border-none text-gray-100 rounded-l-none"
         type="submit"
         value="Copy"
         ref="btncopy"
         @click="copy"
       />
     </div>
-    <div class="w-full border rounded border-gray-600">
-        <p class="text-gray-100 mb-2 px-5 font-semibold" v-text="domain"></p>
-        <a :href="link.url" class="px-5 text-blue-400 mb-2" v-text="link.url"></a>
-        <div class="flex flex-grow items-baseline">
-          <a :href="link.code" class="px-5 text-blue-400 mb-2" v-text="link.shortenLink"></a>
-          <button
-            class="cursor-pointer border p-1 pt-0 rounded m-2 bg-blue-400 outline-none border-none text-gray-100"
-            @click="copy"
-          >Copy</button>
-          <button
-            class="cursor-pointer border p-1 pt-0 rounded m-2 bg-blue-400 outline-none border-none text-gray-100"
-            @click="share = true"
-          >Share</button>
-        </div>
+    <div class="w-full flex flex-col border rounded border-gray-600">
+      <p class="text-gray-100 mb-2 px-5 font-semibold" v-text="domain"></p>
+      <a :href="link.url" class="px-5 text-blue-400 mb-2" v-text="link.url"></a>
+      <a :href="link.shortenLink" class="px-5 text-blue-400 mb-2" v-text="link.shortenLink"></a>
+      <div class="flex px-5 flex-grow items-baseline">
+        <button
+          class="bg-blue-100 active:bg-blue-200 text-blue-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md font-bold text-xs"
+          @click="copy"
+        >Copy</button>
+        <button
+          class="bg-blue-100 active:bg-blue-200 text-blue-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md font-bold text-xs"
+          @click="shareLink"
+        >Share</button>
+      </div>
     </div>
-    <share v-if="share" @close="share=false" :data="link" @copy="copy"></share>
+    <share v-if="sharefallback" @close="sharefallback=false" :data="link" @copy="copy"></share>
   </div>
 </template>
 <script>
@@ -43,7 +43,7 @@ export default {
   data() {
     return {
       link: this.data,
-      share: false
+      sharefallback: false
     };
   },
   computed: {
@@ -60,6 +60,22 @@ export default {
       if (document.execCommand("copy")) {
         this.$refs.btncopy.value = "Copied";
         window.navigator.vibrate(200);
+      }
+    },
+    shareLink() {
+      if (navigator.share) {
+        navigator
+          .share({
+            title: "theImp URL Shortener",
+            text: "Check out " + this.domain,
+            url: this.link.shortenLink
+          })
+          .then(() => console.log("Successful share"))
+          .catch(error => {
+            console.log("Error sharing", error);
+          });
+      } else {
+        this.sharefallback = true;
       }
     }
   }
